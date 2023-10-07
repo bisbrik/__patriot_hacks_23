beef40459a9476986352285b1807cf20f04a13d8
Devnhe <98574776+Devnhe@users.noreply.github.com>
2023-10-07 16:58:17 -0400

    Add files via upload

--- hacks_weather.py
+++ hacks_weather.py
@@ -1,75 +1,67 @@
 import requests
 import json
 
-zipcode = input("enter zipcode: ")
-url = f"https://api.tomorrow.io/v4/weather/forecast?location={zipcode}&US&apikey=gidPJT2TE5dTjbryyiof53743SJp3duL"
-headers = {"accept": "application/json"}
-response = requests.get(url, headers=headers)
-data = response.json()
-
-
-print(data['timelines']['hourly'][0])
-
-
-def avg_temperature () :
-    output = 0
-    for i in range(8) :
-        output = output + (data['timelines']['hourly'][i]['values']['temperature'])
-    output = output / 8
-    return output
-
-def avg_temperatureApparent () :
-    output = 0
-    for i in range(8) :
-        output = output + (data['timelines']['hourly'][i]['values']['temperatureApparent'])
-    output = output / 8
-    return output
+class weather_API:
     
-def avg_humidity () :
-    output = 0
-    for i in range(8) :
-        output = output + (data['timelines']['hourly'][i]['values']['humidity'])
-    output = output / 8
-    return output
+    def __init__(self):
+        self.avg_temperature = 0
+        self.avg_temperatureApparent = 0
+        self.avg_humidity = 0
+        self.avg_rainAccumulation = 0
+        self.avg_snowAccumulation = 0
+        self.avg_uvIndex = 0
+        self.avg_windGust = 0
+        self.avg_windSpeed = 0
+        self.if_uvHealthConcern = 0
+    
+    def get_averages (self, zipcode):
+        url = f"https://api.tomorrow.io/v4/weather/forecast?location={zipcode}&US&apikey=gidPJT2TE5dTjbryyiof53743SJp3duL"
+        headers = {"accept": "application/json"}
+        for i in range(8) :
+            response = requests.get(url, headers=headers)
+            data = response.json()
+            self.avg_temperature += (data['timelines']['hourly'][i]['values']['temperature'])
+            self.avg_temperatureApparent += (data['timelines']['hourly'][i]['values']['temperatureApparent'])
+            self.avg_humidity += (data['timelines']['hourly'][i]['values']['humidity'])
+            self.avg_rainAccumulation += (data['timelines']['hourly'][i]['values']['rainAccumulation'])
+            self.avg_snowAccumulation += (data['timelines']['hourly'][i]['values']['snowAccumulation'])
+            self.avg_uvIndex += (data['timelines']['hourly'][i]['values']['uvIndex'])
+            self.avg_windGust += (data['timelines']['hourly'][i]['values']['windGust'])
+            self.avg_windSpeed += (data['timelines']['hourly'][i]['values']['windSpeed'])
+            if (data['timelines']['hourly'][i]['values']['windSpeed']) == 1:
+                self.if_uvHealthConcern = 1
+        self.avg_temperature /= 8
+        self.avg_temperatureApparent /= 8
+        self.avg_humidity /= 8
+        self.avg_rainAccumulation /= 8
+        self.avg_snowAccumulation /= 8
+        self.avg_uvIndex /= 8
+        self.avg_windGust /= 8
+        self.avg_windSpeed /= 8
+        
+    def get_avg_temperature (self):
+        return self.avg_temperature
 
-def avg_rainAccumulation () :
-    output = 0
-    for i in range(8) :
-        output = output + (data['timelines']['hourly'][i]['values']['rainAccumulation'])
-    output = output / 8
-    return output
+    def get_avg_temperatureApparent (self):
+        return self.avg_temperatureApparent
     
-def avg_snowAccumulation () :
-    output = 0
-    for i in range(8) :
-        output = output + (data['timelines']['hourly'][i]['values']['snowAccumulation'])
-    output = output / 8
-    return output
+    def get_avg_humidity (self):
+        return self.avg_humidity
     
-def avg_uvIndex () :
-    output = 0
-    for i in range(8) :
-        output = output + (data['timelines']['hourly'][i]['values']['uvIndex'])
-    output = output / 8
-    return output
+    def get_avg_rainAccumulation (self):
+        return self.avg_rainAccumulation
     
-def avg_windGust () :
-    output = 0
-    for i in range(8) :
-        output = output + (data['timelines']['hourly'][i]['values']['windGust'])
-    output = output / 8
-    return output
+    def get_avg_snowAccumulation (self):
+        return self.avg_snowAccumulation
     
-def avg_windSpeed () : 
-    output = 0
-    for i in range(8) :
-        output = output + (data['timelines']['hourly'][i]['values']['windSpeed'])
-    output = output / 8
-    return output
-
-def if_uvHealhConcern () :
-    output = 0
-    for i in range(8) :
-        if (data['timelines']['hourly'][i]['values']['windSpeed']) == 1 :
-            output = 1
-    return output
+    def get_avg_uvIndex (self):
+        return self.avg_uvIndex
+    
+    def get_avg_windGust (self):
+        return self.avg_windGust
+    
+    def get_avg_windSpeed (self):
+        return self.avg_windSpeed
+    
+    def get_if_uvHealthConcern (self):
+        return self.if_uvHealthConcern
